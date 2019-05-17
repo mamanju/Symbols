2cba13b92446bc166eb4958a1baf9b0541077258
ryusuke <ryusuke0428tora@gmail.com>
2019-05-17 09:35:31 +0900

    敵の動き方とアイテムドロップシステム

---
+++ SymbolProject/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public int enemyhp;
+    public GameObject crystalToSpawn;
+
+    //敵は死んでから、クリスタルドロップされる
+    public void hurtEnemy(int playeratk)
+    {
+        enemyhp -= playeratk;
+        if (enemyhp <= 0)
+        {
+            Destroy(gameObject);
+            Instantiate(crystalToSpawn, transform.position, Quaternion.identity);
+        }
+
+    }
+}
---
+++ SymbolProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playeratk = 1;
+    public static PlayerController instance;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<EnemyController>().hurtEnemy(playeratk); 
+        }
+    }
+}
