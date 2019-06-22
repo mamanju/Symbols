727fd8f3e60ba1e34fda294c772daff03ee82ff5
DavidDuarte1993 <49542308+DavidDuarte1993@users.noreply.github.com>
2019-06-22 15:59:06 +0900

    Add files via upload

---
+++ Assets(0622)/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public int enemyhp;
+    public int enemyatk;
+    public static EnemyController instance;
+    public GameObject crystalToSpawn;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //敵は死んでから、クリスタルドロップされる
+    public void hurtEnemy(int playeratk)
+    {
+        enemyhp -= playeratk;
+        if (enemyhp <= 0)
+        {
+            Destroy(gameObject);
+            Instantiate(crystalToSpawn, transform.position, Quaternion.identity);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            hurtEnemy(PlayerController.instance.playeratk);
+        }
+            
+    }
+
+}
