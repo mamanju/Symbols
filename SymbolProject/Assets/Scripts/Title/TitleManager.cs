9c2492e2d98b2e38d0ff391be2d149855a05ca51
mamanju <himazin19970630@gmail.com>
2019-06-28 04:47:44 +0900

    InputManager調整、スクリプト修正

--- SymbolProject/Assets/Scripts/Title/TitleManager.cs
+++ SymbolProject/Assets/Scripts/Title/TitleManager.cs
@@ -15,48 +15,48 @@ public class TitleManager : MonoBehaviour
     }
 
 
-    private float Horizontal_L;
-    private float Vertical_L;
-    private float Horizontal_R;
-    private float Vertical_R;
-    private void Update()
-    {
-        Horizontal_L = Input.GetAxis("Horizontal_L");
-        Vertical_L = Input.GetAxis("Vertical_L");
-        Horizontal_R = Input.GetAxis("Horizontal_R");
-        Vertical_R = Input.GetAxis("Vertical_R");
+    //private float Horizontal_L;
+    //private float Vertical_L;
+    //private float Horizontal_R;
+    //private float Vertical_R;
+    //private void Update()
+    //{
+    //    Horizontal_L = Input.GetAxis("Horizontal_L");
+    //    Vertical_L = Input.GetAxis("Vertical_L");
+    //    Horizontal_R = Input.GetAxis("Horizontal_R");
+    //    Vertical_R = Input.GetAxis("Vertical_R");
 
-        // if () { return; }
+    //    // if () { return; }
 
-        Debug.Log("Horizon_L = " + Horizontal_L);
-        Debug.Log("Horizon_R = " + Horizontal_R);
-        Debug.Log("vertical_L = " + Vertical_L);
-        Debug.Log("vertical_R = " + Vertical_R);
+    //    Debug.Log("Horizon_L = " + Horizontal_L);
+    //    Debug.Log("Horizon_R = " + Horizontal_R);
+    //    Debug.Log("vertical_L = " + Vertical_L);
+    //    Debug.Log("vertical_R = " + Vertical_R);
         
-        if (Input.GetButtonDown("Cross"))
-        {
-            Debug.Log("×");
-        }
-        if (Input.GetButtonDown("Circle"))
-        {
-            Debug.Log("〇");
-        }
-        if (Input.GetButtonDown("Square"))
-        {
-            Debug.Log("□");
-        }
-        if (Input.GetButtonDown("Triangle"))
-        {
-            Debug.Log("△");
-        }
-        if (Input.GetButtonDown("L1"))
-        {
-            Debug.Log("L1");
-        }
-        if (Input.GetButtonDown("R1"))
-        {
-            Debug.Log("R1");
-        }
+    //    if (Input.GetButtonDown("Cross"))
+    //    {
+    //        Debug.Log("×");
+    //    }
+    //    if (Input.GetButtonDown("Circle"))
+    //    {
+    //        Debug.Log("〇");
+    //    }
+    //    if (Input.GetButtonDown("Square"))
+    //    {
+    //        Debug.Log("□");
+    //    }
+    //    if (Input.GetButtonDown("Triangle"))
+    //    {
+    //        Debug.Log("△");
+    //    }
+    //    if (Input.GetButtonDown("L1"))
+    //    {
+    //        Debug.Log("L1");
+    //    }
+    //    if (Input.GetButtonDown("R1"))
+    //    {
+    //        Debug.Log("R1");
+    //    }
         
-    }
+    //}
 }
