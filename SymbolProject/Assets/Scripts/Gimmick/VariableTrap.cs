dbc2444436167e46dae266b6b76fa3db8d4e5c87
tokyotech-hosyu <himazin19970630@gmail.com>
2019-07-01 15:36:59 +0900

    倒れる木マスター

--- SymbolProject/Assets/Scripts/Gimmick/VariableTrap.cs
+++ SymbolProject/Assets/Scripts/Gimmick/VariableTrap.cs
@@ -5,52 +5,14 @@ using UnityEngine;
 //時間で変わる炎のギミックのスクリプト（時間でColliderとEffectはActiveとDesactiveに変わるので、Desactiveの時プレイヤーはダメージを受けないで、進める）
 public class VariableTrap : MonoBehaviour
 {
-    [SerializeField]
-    private Collider firecol;
-
-    private bool activeFlag = false;
-
-    public bool ActiveFlag {
-        get;set;
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        firecol = this.GetComponent<Collider>();
-    }
-
-    public void FireOn()
-    {
-        firecol.gameObject.SetActive(true);
+        
     }
 
     public void FireOff()
     {
-        firecol.gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
-
-    private void OnCollisionStay(Collision col)
-    {
-        if (col.gameObject.CompareTag("Player"))
-        {
-
-        }
-    }
-
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.tag == "Player")
-    //    {
-    //        FireOn();
-    //        PlayerController.instance.playerhp--;
-    //    }
-    //}
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.tag == "Player")
-    //    {
-    //        FireOff();
-    //    }
-    //}
 }
