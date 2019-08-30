96b751190afc209589f9798c935e71fb6e83ad99
mamanju <himazin19970630@gmail.com>
2019-08-30 20:28:25 +0900

    クリスタル破壊処理

--- SymbolProject/Assets/Scripts/Gimmick/VariableTrap/VariableTrapSwitch.cs
+++ SymbolProject/Assets/Scripts/Gimmick/VariableTrap/VariableTrapSwitch.cs
@@ -7,13 +7,34 @@ public class VariableTrapSwitch : MonoBehaviour
     [SerializeField]
     private GameObject Fires;
 
+    private float deleteDistance = 2f;
+    private float deleteTime = 2f;
+
     public void StopFire() {
         for(int i = 0; i < Fires.transform.childCount; i++) {
             if (Fires.transform.GetChild(i).GetChild(0).GetComponent<VariableTrap>()) {
                 Debug.Log("OK");
                 Destroy(Fires.transform.GetChild(i).GetChild(0).GetComponent<VariableTrap>().gameObject);
             }
-            //Fires.transform.GetChild(i).GetChild(0).GetComponent<VariableTrap>().FireOff();
+            Fires.transform.GetChild(i).GetChild(0).GetComponent<VariableTrap>().FireOff();
         }
+
+        
+    }
+
+    private IEnumerator DeleteCrystal()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject crystal = transform.GetChild(i).gameObject;
+            crystal.GetComponent<Rigidbody>().isKinematic = false;
+        }
+        yield return new WaitForSeconds(deleteDistance);
+        
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        yield return null;
     }
 }
