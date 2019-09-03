73b71938cdd22812d99814710f867746498f2234
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-09-03 10:04:22 +0900

    矢印の途中

---
+++ SymbolProject/Assets/Scripts/Others/Destination.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform cursor;
+    [SerializeField]
+    private Transform growTree;
+    [SerializeField]
+    private Transform climbTree;
+    [SerializeField]
+    private Transform firstBridge;
+    [SerializeField]
+    private Transform secondBridge;
+    [SerializeField]
+    private Transform crystal;
+    [SerializeField]
+    private Transform bossGate;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+}
